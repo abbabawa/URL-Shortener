5c1bbeed4806db0cf36ea2a11e59321a38bfdca6
Bawa Abba <abbabawa6@gmail.com>
2021-04-23 19:23:02 +0100

    final commit

--- server.js
+++ server.js
@@ -2,17 +2,27 @@ var express = require('express');
 var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+const path = require("path");
+
 var schema = require('./schemas/schema');
 
 let urlInputs = require("./db/data_store")
 
 var root = require("./resolvers/resolvers")
 var app = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
 app.use('/graphiql', graphqlHTTP({
   schema: schema,
   rootValue: root,
   graphiql: true,
 }));
+
+app.get('/', (req, res)=>{
+	res.render("index", {data: urlInputs})
+})
 
 app.get('/:code', (req, res)=>{
 	if (!urlInputs[req.params.code]) {
